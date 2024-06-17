536181a237ff25eb107c4502cf99e00e0a0ee31e
khushbii683 <btech10351.19@bitmesra.ac.in>
2024-06-17 17:12:55 +0530

    added langchain model

--- config/langchain.js
+++ config/langchain.js
@@ -1,27 +1,21 @@
-// langchain.js
-require('dotenv').config();
-const axios = require('axios');
+  //Import the OpenAPI Large Language Model (you can import other models here eg. Cohere)
+  import { OpenAI } from "langchain/llms";
 
-const langchainApiKey = process.env.LANGCHAIN_API_KEY;
-const langchainApiUrl = 'https://api.langchain.com/v1/endpoint'; // replace with the actual endpoint
+  //Load environment variables (populate process.env from .env file)
+  import * as dotenv from "dotenv";
+  dotenv.config();
 
-async function getLangChainResponse(input) {
-    try {
-        const response = await axios.post(
-            langchainApiUrl,
-            { input },
-            {
-                headers: {
-                    'Authorization': `Bearer ${langchainApiKey}`,
-                    'Content-Type': 'application/json'
-                }
-            }
-        );
-        return response.data;
-    } catch (error) {
-        console.error('Error interacting with LangChain API:', error);
-        throw error;
-    }
-}
+  export const getLangChainResponse = async (question) => {
 
-module.exports = { getLangChainResponse };
+      //Instantiante the OpenAI model 
+      //Pass the "temperature" parameter which controls the RANDOMNESS of the model's output. A lower temperature will result in more predictable output, while a higher temperature will result in more random output. The temperature parameter is set between 0 and 1, with 0 being the most predictable and 1 being the most random
+      const model = new OpenAI({ temperature: 0.9 });
+
+      //Calls out to the model's (OpenAI's) endpoint passing the prompt. This call returns a string
+      const res = await model.call(
+         question
+      );
+      consol.log("response ai", res)
+      return res
+  };
+
